dd327366c545ea1009defb0e331a10dfe967f871
Milo2003 <milonorenalo@gmail.com>
2023-04-10 15:14:53 -0500

    Configuracion inicial del proyecto

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>React + TypeScript</title>
+        <meta name='description' content='Genererated by Milo2003'/>
+      </Head>
       <body>
         <Main />
         <NextScript />
